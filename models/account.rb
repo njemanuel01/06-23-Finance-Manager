0f51ad84c2f0b250126abf55dc1ad00700656ff6
Nicholas Emanuel <njemanuel01@gmail.com>
2015-06-24 09:41:19 -0500

    Added accounts model and views.

---
+++ models/account.rb
@@ -0,0 +1,11 @@
+class Account
+  def initialize(values = {})
+    @id = values["id"]
+    @name = values["name"]
+  end
+  
+  def total_balance
+    #method that adds the balances of all the accounts associated with the user
+  end
+  
+end
