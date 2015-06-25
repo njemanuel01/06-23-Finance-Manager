2a647d78efc1e56fe4826c5590804a38aca80ae0
Nicholas Emanuel <njemanuel01@gmail.com>
2015-06-25 12:36:07 -0500

    Added minitests for models.

---
+++ tests/account.test.rb
@@ -0,0 +1,27 @@
+require "minitest/autorun"
+require_relative "../models/category.rb"
+
+class CategoryTest < Minitest::Test
+  #test the update_balance method updates the balance correctly
+  def test_update_balance
+    hash = {"balance" => 10}
+    test = Account.new(hash)
+    test.update_balance(-5, -10)
+    assert_equal(5, test.balance)
+  end
+  
+  #test the transaction adds to the balance correctly
+  def test_transaction
+    hash = {"balance" => 0}
+    test = Account.new(hash)
+    test.transaction(10)
+    assert_equal(10, test.balance)
+  end
+  
+  #test the delete? method returns a correct boolean
+  def test_delete?
+    hash = {"balance" => 0}
+    test = Account.new(hash)
+    assert(test.valid?)
+  end
+end
