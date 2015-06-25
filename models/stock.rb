965fdac9ef2e2622ac016b8f4b98f673ffff8629
Nicholas Emanuel <njemanuel01@gmail.com>
2015-06-25 14:42:54 -0500

    Added test for stock model

--- models/stock.rb
+++ models/stock.rb
@@ -2,7 +2,11 @@ require "stock_quote"
 
 class Stock
   attr_reader :symbol, :bid, :change, :change_percent, :day_range
-
+  # Creates a new Stock object
+  #
+  # symbol - string for the company stock symbol
+  #
+  # Returns an object of the Stock class
   def initialize(symbol)
     @stock = StockQuote::Stock.quote(symbol)
     @symbol = @stock.symbol
@@ -11,7 +15,10 @@ class Stock
     @change_percent = @stock.change_percent_change
     @day_range = @stock.days_range
   end
-
+  
+  # Checks to see if @stock stores information on a valid company
+  #
+  # Returns a string
   def error
     if @stock.response_code = 404
       "Invalid stock symbol"
