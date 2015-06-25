f792d9e639e55d9b8882d582e2bf1332b0029d02
Nicholas Emanuel <njemanuel01@gmail.com>
2015-06-25 11:02:41 -0500

    Added namespacing to transaction controller

--- controllers/transactions.rb
+++ controllers/transactions.rb
@@ -10,11 +10,11 @@ end
 
 # Add transaction to table
 get "/new_transaction_form_do" do
-  account = Account.find(params["account_id"])
-  account.transaction(params["amount"].to_f)
+  account = Account.find(params["transaction"]["account_id"])
+  account.transaction(params["transaction"]["amount"].to_f)
   account.save
-  transaction = Transaction.add({"amount" => params["amount"].to_f, "description" => params["description"], "date" => params["date"], 
-    "category_id" => params["category_id"], "account_id" => params["account_id"]})
+  transaction = Transaction.add({"amount" => params["transaction"]["amount"].to_f, "description" => params["transaction"]["description"], "date" => params["transaction"]["date"], 
+    "category_id" => params["transaction"]["category_id"], "account_id" => params["transaction"]["account_id"]})
   # update account balance
   erb :"transactions/added"
 end
@@ -42,35 +42,35 @@ end
 
 # Save updates to table
 get "/update_transaction_form_do" do
-  transaction = Transaction.find(params["id"])
+  transaction = Transaction.find(params["transaction"]["id"])
   @message = []
-  if params["account_id"] != "blank"
+  if params["transaction"]["account_id"] != "blank"
     account = Account.find(transaction.account_id)
     account.update_balance(transaction.amount)
     account.save
-    transaction.account_id = params["account_id"]
+    transaction.account_id = params["transaction"]["account_id"]
     account = Account.find(transaction.account_id)
     account.transaction(transaction.amount)
     account.save
     @message << "Transaction account updated."
   end
-  if params["category_id"] != "blank"
-    transaction.category_id = params["category_id"]
+  if params["transaction"]["category_id"] != "blank"
+    transaction.category_id = params["transaction"]["category_id"]
     @message << "Transaction category updated."
   end
-  if params["amount"] != ""
+  if params["transaction"]["amount"] != ""
     account = Account.find(transaction.account_id)
-    account.update_balance(transaction.amount, params["amount"].to_f)
+    account.update_balance(transaction.amount, params["transaction"]["amount"].to_f)
     account.save
-    transaction.amount = params["amount"].to_f
+    transaction.amount = params["transaction"]["amount"].to_f
     @message << "Transaction amount updated."
   end
-  if params["date"] != ""
-    transaction.date = params["date"]
+  if params["transaction"]["date"] != ""
+    transaction.date = params["transaction"]["date"]
     @message << "Transaction date updated."
   end
-  if params["description"] != ""
-    transaction.description = params["description"]
+  if params["transaction"]["description"] != ""
+    transaction.description = params["transaction"]["description"]
     @message << "Transaction description updated."
   end
   
@@ -85,7 +85,7 @@ end
 
 # Deletes transaction from table
 get "/delete_transaction" do
-  transaction = Transaction.find(params["id"])
+  transaction = Transaction.find(params["transaction"]["id"])
   account = Account.find(transaction.account_id)
   account.update_balance(transaction.amount)
   account.save
