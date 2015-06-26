04e83128fd6255e2a4b344d08c34069aadecb02f
Nicholas Emanuel <njemanuel01@gmail.com>
2015-06-26 15:48:47 -0500

    Made create_hash method in instance methods

--- database_instance_methods.rb
+++ database_instance_methods.rb
@@ -3,7 +3,6 @@ require "active_support/inflector"
 
 #Creates instance methods to access the database with.
 module DatabaseInstanceMethod
-  
   # Creates a string for the calling classes table
   #
   # Returns a string
@@ -23,35 +22,31 @@ module DatabaseInstanceMethod
   #
   # Returns a string.
   def save
-    hash = {}
-    hash = self.create_attr_hash(hash)
-    hash = self.delete_attr(hash)
+    hash = self.attr_hash
     sql_hash = hash.to_s.delete "\>"
 
     CONNECTION.execute("UPDATE '#{tablename}' SET #{sql_hash[1...-1]} WHERE id = ?;", @id)
     "Saved."
   end
   
-  # Deletes unneeded values from a Hash
+  # Creates a Hash from the instances attributes
   #
-  # hash - Hash
-  # 
   # Returns a Hash
-  def delete_attr(hash)
+  def attr_hash
+    hash = create_hash
     hash.delete("id")
     hash.delete("errors")
     return hash
   end
   
-  # Creates a Hash of all an instances attributes
-  #
-  # hash - Hash
+  # Creates a Hash 
   #
   # Returns a Hash
-  def create_attr_hash(hash)
+  def create_hash
+    hash = {}
     self.instance_variables.each {|var| hash[var.to_s.delete("@")] = self.instance_variable_get(var) }
+    return hash
   end
-    
   
   # Deletes a row from a table
   #
@@ -65,11 +60,9 @@ module DatabaseInstanceMethod
   #
   # Returns a Boolean.
   def add_to_database
-    hash = self.create_attr_hash(hash)
-    hash = self.delete_attr(hash)
+    hash = self.attr_hash
     columns = hash.keys
     values = hash.values
-    binding.pry
     if self.valid?
       CONNECTION.execute("INSERT INTO #{tablename} (#{columns.join ", "}) VALUES (#{values.to_s[1...-1]});")
       @id = CONNECTION.last_insert_row_id
