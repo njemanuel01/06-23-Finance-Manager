c2c031e43ff0bdb6432907193980b19201cab9f3
Nicholas Emanuel <njemanuel01@gmail.com>
2015-07-18 12:37:57 -0500

    Finished transactions view update

--- controllers/transactions.rb
+++ controllers/transactions.rb
@@ -19,9 +19,10 @@ end
 
 # Add transaction to table
 get "/new_transaction_form_do" do
+  check_user
   transaction = Transaction.new({"id" => nil, "amount" => params["transaction"]["amount"].to_f, "description" => params["transaction"]["description"], "date" => params["transaction"]["date"], 
     "category_id" => params["transaction"]["category_id"], "account_id" => params["transaction"]["account_id"]})
-  if transaction.valid?
+  if transaction.amount_not_zero?
     account = Account.find(params["transaction"]["account_id"])
     account.transaction(params["transaction"]["amount"].to_f)
     account.save
@@ -43,24 +44,30 @@ end
 
 # Get information on a single transaction
 get "/single_transaction/:id" do
+  check_user
   @transaction = Transaction.find(params["id"])
   erb :"transactions/transaction_single"
 end
 
 # Get a list of transactions for a specific account
 get "/where" do
+  check_user
+  @user = User.find(session["id"])
   erb :"transactions/where_account"
 end
 
 # Form to update transactions with
 get "/update_transaction_form" do
+  check_user
+  @user = User.find(session["id"])
   erb :"transactions/update_transaction_form"
 end
 
 # Save updates to table
 get "/update_transaction_form_do" do
+  check_user
   transaction = Transaction.find(params["transaction"]["id"])
-  @message = []
+  @errors = []
   if params["transaction"]["account_id"] != "blank"
     account = Account.find(transaction.account_id)
     account.update_balance(transaction.amount)
@@ -69,43 +76,47 @@ get "/update_transaction_form_do" do
     account = Account.find(transaction.account_id)
     account.transaction(transaction.amount)
     account.save
-    @message << "Transaction account updated."
+    @errors << "Transaction account updated."
   end
   if params["transaction"]["category_id"] != "blank"
     transaction.category_id = params["transaction"]["category_id"]
-    @message << "Transaction category updated."
+    @errors << "Transaction category updated."
   end
   if params["transaction"]["amount"] != ""
     account = Account.find(transaction.account_id)
     account.update_balance(transaction.amount, params["transaction"]["amount"].to_f)
     account.save
     transaction.amount = params["transaction"]["amount"].to_f
-    @message << "Transaction amount updated."
+    @errors << "Transaction amount updated."
   end
   if params["transaction"]["date"] != ""
     transaction.date = params["transaction"]["date"]
-    @message << "Transaction date updated."
+    @errors << "Transaction date updated."
   end
   if params["transaction"]["description"] != ""
     transaction.description = params["transaction"]["description"]
-    @message << "Transaction description updated."
+    @errors << "Transaction description updated."
   end
   
   transaction.save
-  erb :"transactions/updated"
+  erb :"transactions/transactions_menu"
 end
 
 # Choose a transaction to delete
 get "/delete_transaction_list" do
+  check_user
+  @user = User.find(session["id"])
   erb :"transactions/delete_transaction_list"
 end
 
 # Deletes transaction from table
 get "/delete_transaction" do
+  check_user
   transaction = Transaction.find(params["transaction"]["id"])
   account = Account.find(transaction.account_id)
   account.update_balance(transaction.amount)
   account.save
   transaction.delete
-  erb :"transactions/deleted"
+  @errors = "Transaction deleted."
+  erb :"transactions/transactions_menu"
 end
--- models/transaction.rb
+++ models/transaction.rb
@@ -1,10 +1,9 @@
 class Transaction< ActiveRecord::Base
-  validates :amount, numericality: true, if: :amount_not_zero?
+  validates :amount, numericality: true
   validates :date, presence: true
   validates :date, presence: true
   
   def amount_not_zero?
-    binding.pry
     self.amount != 0
   end
 end
